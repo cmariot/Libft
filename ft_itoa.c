f5c3cc7bb70faa708aba98e8914a70b203ce6098
Charles Mariot <contact@charles-mariot.fr>
2021-04-26 13:48:55 +0200

    ft_itoa ✅

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: cmariot <cmariot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/19 12:17:38 by cmariot           #+#    #+#             */
-/*   Updated: 2021/04/25 17:20:23 by cmariot          ###   ########.fr       */
+/*   Updated: 2021/04/26 13:48:07 by cmariot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,8 @@
 #include "stdio.h"
 #include <stdlib.h>
 
-char	*ft_itoa(int n)
+char	*special_case_itoa(int n)
 {
-	int		n_cpy;
-	int		n_len;
-	char	*nombre;
-	int		signe;
 	char	*special_cas;
 
 	if (n == 0)
@@ -30,7 +26,7 @@ char	*ft_itoa(int n)
 		special_cas = ft_memcpy(special_cas, "0", 1);
 		return (special_cas);
 	}
-	if (n == -2147483648)
+	else if (n == -2147483648)
 	{
 		special_cas = malloc(sizeof(char) * 12);
 		if (special_cas == NULL)
@@ -38,23 +34,25 @@ char	*ft_itoa(int n)
 		special_cas = ft_memcpy(special_cas, "-2147483648", 11);
 		return (special_cas);
 	}
-	n_len = 0;
-	signe = 1;
-	if (n < 0)
-	{
-		n = -n;
-		signe++;
-		n_len++;
-	}
-	n_cpy = n;
-	while (n_cpy != 0)
-	{
-		n_cpy = n_cpy / 10;
-		n_len++;
-	}
-	nombre = malloc(sizeof(char) * (n_len + signe));
-	if (nombre == NULL)
+	else
 		return (NULL);
+}
+
+int	int_len(int n)
+{
+	int	n_len;
+
+	n_len = 0;
+	while (n != 0)
+	{
+		n = n / 10;
+		n_len++;
+	}
+	return (n_len);
+}
+
+char	*int_to_array(int signe, char *nombre, int n, int n_len)
+{
 	nombre[n_len] = '\0';
 	if (signe == 2)
 		nombre[0] = '-';
@@ -64,5 +62,30 @@ char	*ft_itoa(int n)
 		n = n / 10;
 		n_len--;
 	}
+	return (nombre);
+}
+
+char	*ft_itoa(int n)
+{
+	int		n_len;
+	char	*nombre;
+	int		signe;
+	char	*special_case;
+
+	special_case = special_case_itoa(n);
+	if (special_case != NULL)
+		return (special_case);
+	n_len = int_len(n);
+	signe = 1;
+	if (n < 0)
+	{
+		n = -n;
+		signe++;
+		n_len++;
+	}
+	nombre = malloc(sizeof(char) * (n_len + signe));
+	if (nombre == NULL)
+		return (NULL);
+	nombre = int_to_array(signe, nombre, n, n_len);
 	return (nombre);
 }
