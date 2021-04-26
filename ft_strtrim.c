c79e47d74616d94a47776583211acab9ff9bbd62
Charles Mariot <contact@charles-mariot.fr>
2021-04-26 12:21:23 +0200

    ft_strtrim.c ✅

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,29 +6,25 @@
 /*   By: cmariot <cmariot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/19 14:13:45 by cmariot           #+#    #+#             */
-/*   Updated: 2021/04/20 20:04:15 by cmariot          ###   ########.fr       */
+/*   Updated: 2021/04/26 12:18:19 by cmariot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-char *ft_strtrim(char const *s1, char const *set)
+char	*ft_strtrim(char const *s1, char const *set)
 {
-	char		*str;
-	int		s1_len;
-	unsigned int	start;
+	char			*str;
 	unsigned int	end;
 
 	if (!s1 || !set)
 		return (NULL);
-	s1_len = ft_strlen(s1);
-	start = 0;
-	if (ft_memcmp(s1, set, ft_strlen(set)) == 0)
-		start = ft_strlen(set);
+	while (*s1 && ft_strchr(set, *s1))
+		s1++;
 	end = ft_strlen(s1);
 	while (end != 0 && ft_strchr(set, s1[end]))
 		end--;
-	str = ft_substr((char*)s1, (0 + start), (end + 1));
+	str = ft_substr((char *)s1, 0, end + 1);
 	return (str);
 }
