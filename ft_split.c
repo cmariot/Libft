2949b0a6bacc5b12df27dea3bc2f47913c641e5a
Charles Mariot <contact@charles-mariot.fr>
2021-04-26 17:13:43 +0200

    ft_split

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: cmariot <cmariot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/16 13:14:08 by cmariot           #+#    #+#             */
-/*   Updated: 2021/04/25 17:10:56 by cmariot          ###   ########.fr       */
+/*   Updated: 2021/04/26 17:02:03 by cmariot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,33 +14,41 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-char	**ft_split(char const *s, char c)
+int	count_words(char const *s, char c)
 {
-	char	**strs;
 	int	i;
-	int	j;
-	int	k;
 	int	words;
 
 	i = 0;
-	words = 1;
-	while (s[i] == c)
+	words = 0;
+	if (s == NULL)
+		return (0);
+	while (s[i] && s[i] == c)
 		i++;
-	while (s[i] != '\0')
+	while (s[i])
 	{
 		if (s[i] == c)
 		{
+			words++;
 			while (s[i] == c)
 				i++;
-			words++;	
 		}
 		i++;
 	}
-	if (!(strs = malloc(sizeof(char *) * (words))))
-		return (NULL);
+	if (s[i - 1] != c)
+		words++;
+	return (words);
+}
+
+char	**malloc_strs(char const *s, char c, char **strs)
+{
+	int	i;
+	int	j;
+	int	word_len;
+
 	i = 0;
 	j = 0;
-	k = 0;
+	word_len = 0;
 	while (s[i] == c)
 		i++;
 	while (s[i] != '\0')
@@ -48,34 +56,59 @@ char	**ft_split(char const *s, char c)
 		i++;
 		if (s[i] == c || s[i] == '\0')
 		{
-		 	if (!(strs[j] = malloc(sizeof(unsigned char) * (k + 1))))
+			strs[j] = malloc(sizeof(char) * (word_len + 1));
+			if (strs[j] == NULL)
 				return (NULL);
 			while (s[i] == c)
 				i++;
 			j++;
-			k = 0;
+			word_len = 0;
 		}
-		k++;
+		word_len++;
 	}
+	return (strs);
+}
+
+char	**fill_strs(char const *s, char c, char **strs, int words)
+{
+	int	i;
+	int	j;
+	int	word_len;
+
 	i = 0;
 	j = 0;
-	k = 0;
+	word_len = 0;
 	while (s[i] == c)
 		i++;
 	while (s[i] != '\0' && j != words)
 	{
-		strs[j][k] = (unsigned char)s[i];
+		strs[j][word_len] = (unsigned char)s[i];
 		i++;
-		k++;
-		if (s[i] == c || s[i] == '\0')
+		word_len++;
+		if (s[i] == c)
 		{
 			while (s[i] == c)
 				i++;
-			strs[j][k] = '\0';
+			strs[j][word_len] = '\0';
 			j++;
-			k = 0;
+			word_len = 0;
 		}
-	}
-	
+	}	
+	return (strs);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**strs;
+	int		i;
+	int		words;
+
+	i = 0;
+	words = count_words(s, c);
+	strs = malloc(sizeof(char *) * (words + 1));
+	if (strs == NULL)
+		return (NULL);
+	strs = malloc_strs(s, c, strs);
+	strs = fill_strs(s, c, strs, words);
 	return (strs);
 }
--- libft.h
+++ libft.h
@@ -17,20 +17,20 @@
 # include <stdio.h>
 
 size_t		ft_strlen(const char *s);
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-int		ft_toupper(int c);
-int		ft_tolower(int c);
+int			ft_isalpha(int c);
+int			ft_isdigit(int c);
+int			ft_isalnum(int c);
+int			ft_isascii(int c);
+int			ft_isprint(int c);
+int			ft_toupper(int c);
+int			ft_tolower(int c);
 char		*ft_strchr(const char *s, int c);
 char		*ft_strrchr(const char *s, int c);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int			ft_strncmp(const char *s1, const char *s2, size_t n);
 size_t		ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t		ft_strlcat(char *dest, char *src, size_t dest_size);
 char		*ft_strnstr(const char *str, const char *to_find, size_t len);
-int		ft_atoi(const char *str);
+int			ft_atoi(const char *str);
 void		*ft_memset(void *b, int c, size_t len);
 void		ft_bzero(void *b, size_t n);
 void		*ft_memcpy(void *dst, const void *src, size_t n);
@@ -47,7 +47,7 @@ char		**ft_split(char const *s, char c);
 void		*ft_calloc(size_t count, size_t size);
 void		*ft_memmove(void *dst, const void *src, size_t len);
 void		*ft_memchr(const void *s, int c, size_t n);
-int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int			ft_memcmp(const void *s1, const void *s2, size_t n);
 char		*ft_itoa(int n);
 char		*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 
